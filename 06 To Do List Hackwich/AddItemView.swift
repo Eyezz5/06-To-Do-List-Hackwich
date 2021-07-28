cfda5f73fa5060b8869b45b4101aaf990eb316fb
ihateuu <replacement506@gmail.com>
2021-07-28 19:23:08 +0300

    created additional features

---
+++ 06 To Do List Hackwich/AddItemView.swift
@@ -0,0 +1,38 @@
+//
+//  AddItemView.swift
+//  06 To Do List Hackwich
+//
+//  Created by Kareem Almomen on 7/28/21.
+//
+
+import Foundation
+
+struct AddItemView: View {
+    @ObservedObject var toDoList: ToDoList
+    @State private var priority = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+       static let priorities = ["High", "Medium", "Low"]
+    var body: some view {
+        NavigationView {
+            Form {
+                Picker("Priority", selection: $priority) {
+                    ForEach(Self.priorities, id: \.self) {
+                        priorityin in Text(priority)
+                    }
+                }
+                TextField("Description", text: $description)
+                                DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
+
+            }
+            .navigationBarTitle("Add New To Do Item", displayMode: .inline)
+        }
+    }
+}
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView(toDoList)
+    }
+}
+
---
+++ AddItemView.swift
@@ -0,0 +1,8 @@
+//
+//  AddItemView.swift
+//  06 To Do List Hackwich
+//
+//  Created by Kareem Almomen on 7/28/21.
+//
+
+import Foundation
