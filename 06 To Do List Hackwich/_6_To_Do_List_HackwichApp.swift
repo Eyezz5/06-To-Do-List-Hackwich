54a21f47d8154f2a73290d730d3a399f851eb27a
ihateuu <replacement506@gmail.com>
2021-07-28 18:41:25 +0300

    Initial Commit

---
+++ 06 To Do List Hackwich/_6_To_Do_List_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_To_Do_List_HackwichApp.swift
+//  06 To Do List Hackwich
+//
+//  Created by Kareem Almomen on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct _6_To_Do_List_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
